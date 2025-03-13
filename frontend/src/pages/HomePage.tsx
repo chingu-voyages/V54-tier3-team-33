440f8de1a5ca497c887e3f22e763cda18aedae82
ARBUCHELI <radio_seattle@hotmail.com>
2025-03-13 16:21:04 +0300

    feature-header - creating of a first version of the home page

---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../components/Header/Header"
+
+const HomePage: React.FC = () => {
+  return (
+    <>
+      <Header showAdvertising={true}/>
+    </>
+  );
+};
+
+export default HomePage;
