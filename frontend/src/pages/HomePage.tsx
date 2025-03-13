1df120b4a0ebd9d4920cd53d4961340446b52235
ARBUCHELI <radio_seattle@hotmail.com>
2025-03-13 19:56:06 +0300

    feature-header - modifying HomePage for rendering the advertisingCarousel and the ProductList

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,12 +1,28 @@
-import React from "react";
-import Header from "../components/Header/Header"
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Header from "../components/Header/Header";
+import { loadProducts } from "../../src/store/slices/productSlice"; 
+import { RootState, AppDispatch } from "../../src/store/store"
+import ProductList from "../../src/components/ProductList/ProductList";
+import AdvertisingCarousel from "../../src/components/AdvertisingCarousel/AdvertisingCarousel";
 
 const HomePage: React.FC = () => {
-  return (
-    <>
-      <Header showAdvertising={true}/>
-    </>
-  );
+    const dispatch: AppDispatch = useDispatch(); 
+    const { searchResults, loading, error } = useSelector((state: RootState) => state.products);
+
+    useEffect(() => {
+      dispatch(loadProducts()); 
+    }, [dispatch]);
+ 
+    return (
+      <>
+        <Header showAdvertising={true}/>
+        <div className="mb-4 pr-10 pl-10">
+          <AdvertisingCarousel />
+        </div>
+        <ProductList loading={loading} error={error} searchResults={searchResults} />
+      </>
+    );
 };
 
 export default HomePage;
