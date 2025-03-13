63be89caf954ca1bb0a9a7105275f83fb7e94c05
ARBUCHELI <radio_seattle@hotmail.com>
2025-03-13 16:03:45 +0300

    feature-header - adding nav menu with a few first list of categories (Electronics, Clothing, Home, Toys, Sporting Goods, Jewelry & Watches, and first version of configuration of navigation

---
+++ frontend/src/components/Nav/Nav.tsx
@@ -0,0 +1,34 @@
+import { Link } from "react-router-dom"; 
+
+const Nav = () => {
+  const categories = [
+    "Electronics",
+    "Clothing",
+    "Home",
+    "Toys",
+    "Sporting Goods",
+    "Jewelry & Watches",
+  ];
+
+  return (
+    <nav className="bg-white">
+      <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 mb-10">
+        <div className="flex justify-center py-1"> 
+          <div className="flex space-x-18"> 
+            {categories.map((category) => (
+              <Link
+                key={category}
+                to={`/category/${category.toLowerCase().replace(/ & /g, "-").replace(/\s+/g, "-")}`}
+                className="text-gray-700 hover:text-blue-600 transition-colors duration-200"
+              >
+                {category}
+              </Link>
+            ))}
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
