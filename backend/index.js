44b8739413af992c87a8ef13516ccb6b2986fb41
Ismail Marghich <ismailcodes@gmail.com>
2025-03-13 23:57:20 +0100

    fixed cors issue

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,11 @@ require('dotenv').config()
 const express = require('express')
 const Product = require('./models/product')
 const app = express()
+const cors = require("cors");
 const port = process.env.PORT || 3000
 
 
+app.use(cors());
 app.use(express.json())
 
 app.get('/', async (req, res) => {
