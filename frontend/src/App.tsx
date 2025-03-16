c9616443519d0fdfddb7af2adf0d0812ef98d974
ARBUCHELI <radio_seattle@hotmail.com>
2025-03-16 13:58:28 +0300

    feature-header - updating imports in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,6 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CategoryPage from "./pages/CategoryPage"; 
 import "./index.css";
 
 const App = () => {
