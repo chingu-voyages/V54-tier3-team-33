3bcbaaf53d7e24219a9778c546fd10ecc7615a17
TiaDev7474 <nomenjanaharyriry@gmail.com>
2025-03-21 17:41:12 +0300

    feat(auth): store token in response cookies

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -3,10 +3,18 @@ const {verify} = require("jsonwebtoken");
 
 module.exports = {
     authMiddleware: async (req , res , next) =>{
-        const token = req.headers.authorization.split(' ')[1]
-        if(!token) return  res.status(403).json({message:'forbidden'})
-        const decoded =  await verify(token,process.env.JWT_STRONG_SECRET);
-        req.userId = decoded.userId
-        next();
+        const token = req.cookies.token;
+        if(!token) return  res.status(401).json({
+            status: 'failed',
+            message: 'Access denied. No token provided.',
+        });
+        try{
+            const decoded =  await verify(token,process.env.JWT_STRONG_SECRET);
+            req.userId = decoded.userId
+            next();
+        }catch (e) {
+            res.status(403).json({message:'Invalid or expired token'})
+        }
+
     },
 }
--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const authController = require('../controllers/auth.controller')
+const {authMiddleware} = require("../middlewares/auth.middleware");
 
 router.post('/login', authController.login);
 router.post('/register', authController.register);
-
+router.get('/me',authMiddleware, authController.getMe)
 
 module.exports = router;
