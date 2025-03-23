a5919d6225c1efda97c6811914e7865cabe5237a
Masawi <edmasawi@ymail.com>
2025-03-23 12:31:57 +0100

    protect orders route

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,4 @@
-const User =  require('../models/user.model');
+/* const User =  require('../models/user.model');
 const  Opt = require('../models/opt.model');
 const  CustomError= require("../utils/error");
 const jwt = require("jsonwebtoken");
@@ -96,3 +96,106 @@ module.exports = {
         }
     },
 }
+ */
+
+const User = require('../models/user.model');
+const Otp = require('../models/opt.model');
+const CustomError = require("../utils/error");
+const jwt = require("jsonwebtoken");
+
+const { generateOtp } = require("../services/otp/opt.service");
+
+module.exports = {
+    login: async (req, res, next) => {
+        const { email, password } = req.body;
+        try {
+            const foundUser = await User.findOne({ email });
+            if (!foundUser) {
+                const error = new CustomError('User not found', 404);
+                return next(error); 
+            }
+            const validity = await foundUser.comparePassword(password);
+            if (!validity) {
+                const error = new CustomError('Pair identifiant/password not correct', 401);
+                return next(error); 
+            }
+
+            const token = jwt.sign(
+                {
+                    userId: foundUser._id,
+                    isVerified: foundUser.account_verify,
+                },
+                process.env.JWT_STRONG_SECRET,
+                {
+                    expiresIn: process.env.JWT_EXPIRE_TIME
+                }
+            );
+
+            return res.status(201).json({
+                token: token,
+                userId: foundUser._id,
+                message: 'logged in successfully'
+            }); 
+        } catch (e) {
+            return res.status(500).json({
+                status: 'error',
+                message: e.message,
+            });
+        }
+    },
+    register: async (req, res, next) => {
+        const { email, password } = req.body;
+        try {
+            const user = await User.findOne({ email: email });
+            if (user) {
+                return res.status(401).json({ message: 'Email already in use' });
+            }
+
+            const newUser = new User({
+                email: email,
+                password: password
+            });
+
+            const userDoc = await newUser.save();
+            if (!userDoc) {
+                const mongodbError = new CustomError('Unexpected error occurred from MongoDB', 500);
+                return next(mongodbError); 
+            }
+
+            const otp = await generateOtp();
+
+            const newOtp = new Otp({
+                passcode: otp,
+                author: userDoc._id
+            });
+
+            const otpDoc = await newOtp.save();
+            if (!otpDoc) {
+                const mongodbError = new CustomError('Unexpected error occurred from MongoDB', 500);
+                return next(mongodbError); 
+            }
+
+            const token = jwt.sign(
+                {
+                    userId: userDoc._id,
+                    isVerified: userDoc.account_verify,
+                },
+                process.env.JWT_STRONG_SECRET,
+                {
+                    expiresIn: '24h'
+                }
+            );
+
+            return res.status(201).json({
+                status: 'success',
+                message: 'Account created successfully'
+            }); 
+
+        } catch (err) {
+            return res.status(500).json({
+                status: 'error',
+                message: err.message,
+            });
+        }
+    },
+};
--- backend/index.js
+++ backend/index.js
@@ -19,6 +19,8 @@ connectToDatabase()
 
 //Routes
 app.use('/api/auth', require('./routes/auth.routes'))
+app.use('/api/orders', require('./routes/order.routes'));
+
 
 
 app.get('/', async (req, res) => {
