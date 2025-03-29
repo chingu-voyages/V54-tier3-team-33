5bad0fb270242a2e61effa59daeb60a476b4b108
predrag jandric <predrag.jandric.bg@gmail.com>
2025-03-29 23:55:22 +0100

    will ADD: some more polishing and comments about logic implementation to be able to add item to cart and also visit item page

--- frontend/src/components/Card/Card.tsx
+++ frontend/src/components/Card/Card.tsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
 import Button from "../../utils/Button";
 import { BsCartPlus } from "react-icons/bs";
+import { addItemToCart } from "../../store/slices/cartSlice";
+import { useDispatch } from "react-redux";
 
 interface Item {
   id: number;
@@ -14,6 +16,7 @@ function Card({ item }: { item: Item }) {
   // const imageSrc = typeof item.image === 'string'
   //   ? item.image
   //   : item.image[0] || '';
+  const dispatch = useDispatch();
   return (
     // <div className="rounded-custom flex flex-col items-center bg-white p-6 shadow-lg">
     //   {imageSrc && (
@@ -32,21 +35,30 @@ function Card({ item }: { item: Item }) {
 
     // /////////////////////////
 
+    // replace div with link that links to the product page
+    // and pass the product data as state to the product page
     // <Link
-    //   className="text-darktext flex w-64 flex-col items-start gap-1 border border-transparent p-3 transition-all hover:border-gray-200 hover:shadow-lg"
+    //   className="text-darktext flex w-64 cursor-pointer flex-col items-start gap-1 border border-transparent p-3 transition-all hover:border-gray-200 hover:shadow-lg"
     //   to={`/product/${item.id}`}
     //   state={{ product: item }}
     // >
-    <div className="text-darktext flex w-64 cursor-pointer flex-col items-start gap-1 border border-transparent p-3 transition-all hover:border-gray-200 hover:shadow-lg">
-      <img
-        src="https://helios-i.mashable.com/imagery/articles/05djrP5PjtVB7CcMtvrTOAP/images-4.fill.size_2000x1125.v1723100793.jpg"
-        alt="Product Image"
-        className="w-full object-contain"
-      />
+    <div className="text-darktext flex w-72 cursor-pointer flex-col items-start gap-1 border border-transparent p-3 py-4 transition-all hover:border-gray-200 hover:shadow-lg">
+      <div className="relative w-full">
+        <img
+          src="https://helios-i.mashable.com/imagery/articles/05djrP5PjtVB7CcMtvrTOAP/images-4.fill.size_2000x1125.v1723100793.jpg"
+          alt="Product Image"
+          className="w-full rounded-md object-contain"
+        />
 
-      <span className="bg-red-300 rounded-full p-2">
-        <BsCartPlus size={25} />
-      </span>
+        {/* add to cart function to be added */}
+        <button
+          // add toast that item is added to cart
+          onClick={dispatch(addItemToCart(item))}
+          className="hover:bg-darktext absolute right-2 bottom-2 cursor-pointer rounded-full border border-stone-400 bg-white p-2 transition-all hover:text-white"
+        >
+          <BsCartPlus size={25} />
+        </button>
+      </div>
 
       <h3 className="font-medium">HP laptop 15-fc0036nm | HP® 15-fc0036nm</h3>
       <span className="flex w-full items-center gap-1">
