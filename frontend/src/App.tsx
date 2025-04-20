48516b644aed415b2b69acab630b3b6eb89cd7a4
predrag jandric <predrag.jandric.bg@gmail.com>
2025-04-20 17:02:09 +0200

    will FIX: typescript errors

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,19 +8,17 @@ import Layout from "./pages/Layout";
 import SignInPage from "./pages/SignInPage";
 import CreateAccPage from "./pages/CreateAccPage";
 import { useEffect } from "react";
-import {useDispatch } from "react-redux";
-import {fetchAuthenticatedUser } from "./store/slices/authSlice.ts";
-
-
+import { fetchAuthenticatedUser } from "./store/slices/authSlice.ts";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "./store/store"; 
 
 const App = () => {
-  const dispatch = useDispatch();
-
+  const dispatch: AppDispatch = useDispatch()
 
   useEffect(() => {
-     dispatch(fetchAuthenticatedUser())
-  }, []);
-
+    dispatch(() => fetchAuthenticatedUser()); 
+  }, [dispatch]);
+  
   return (
     <Router>
       <Routes>
