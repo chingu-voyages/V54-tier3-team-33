c371822350a4231290da3047e5233d946f64bb59
Masawi <edmasawi@ymail.com>
2025-04-21 09:42:14 +0200

    Logger

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 require("dotenv").config();
 const express = require("express");
+const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
+const {authMiddleware} = require("./middlewares/auth.middleware");
+const loggerMiddleware = require("./middlewares/logger.middleware")
 const globalErrorHandler = require("./controllers/err.controller");
 const Product = require("./models/product.models");
 const path = require("path");
@@ -8,6 +11,7 @@ const path = require("path");
 const app = express();
 const cors = require("cors");
 const { connectToDatabase } = require("./config/db");
+const { log } = require("console");
 const port = process.env.PORT || 3000;
 
 //midlleware
@@ -19,7 +23,8 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(express.static("dist"));
-
+app.use(loggerMiddleware);
+app.use(authMiddleware);
 app.use(globalErrorHandler);
 
 connectToDatabase();
